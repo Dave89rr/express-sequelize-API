e7b3e7a3bb7bc1212e5852185036e578c973350f
David Rivera <daleriro@gmail.com>
2022-06-02 17:33:23 -0400

    swap

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,18 +1,34 @@
 const express = require('express');
-const db = require("../db/models");
+const app = require('../../../d3/authorization-in-express/app');
+const db = require('../db/models');
 const router = express.Router();
 const { Tweet } = db;
-const asyncHandler = require('./utils')
+const asyncHandler = require('./utils');
 
-router.get('/tweets', asyncHandler(async(req, res) => {
-  const allTweets = await Tweet.findAll()
-  res.json(allTweets)
-}))
+// Not Found error
+const tweetNotFoundError = (id) => {
+  const err = new Error(`A tweet of id ${id} could not be found`);
+  err.title = 'Tweet not found';
+  err.status = 404;
+  return err;
+};
 
-router.get('/tweets/:id(\\d+)', asyncHandler(async(req, res) => {
-  const tweet = await Tweet.findByPK(req.params.id)
-  res.json(tweet)
-}))
+router.get(
+  '/tweets',
+  asyncHandler(async (req, res) => {
+    const allTweets = await Tweet.findAll();
+    res.json(allTweets);
+  })
+);
 
+router.get(
+  '/tweets/:id(\\d+)',
+  asyncHandler(async (req, res, next) => {
+    const tweetId = req.params.id;
+    const tweet = await Tweet.findByPk(tweetId);
+    if (tweet !== null) return res.json(tweet);
+    else next(tweetNotFoundError(tweetId));
+  })
+);
 
 module.exports = router;
